f965c60cdc1a8486c34d48c29830c72edf470013
Jeroen <147590275+roener88@users.noreply.github.com>
2024-08-19 11:34:09 +0200

    Added get amenities route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import express from "express";
-import getUsers from "./services/users/getUsers.js";
-
-// Booking
+import getAmenities from "./services/amenities/getAmenities.js";
 import getBookings from "./services/bookings/getBookings.js";
+import getUsers from "./services/users/getUsers.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
+});
+
+// Amenity Routes
+app.get('/amenities', async (req, res) => {
+  try {
+    const amenities = await getAmenities();
+    res.status(200).json(amenities);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong while getting the list of amenities");
+  }
+});
+
+// Booking Routes
+app.get('/bookings', async (req, res) => {
+  try {
+    const bookings = await getBookings();
+    res.status(200).json(bookings);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong while getting the list of bookings");
+  }
 });
 
 // User Routes
@@ -22,16 +43,7 @@ app.get('/users', async (req, res) => {
 });
 
 
-// Booking Routes
-app.get('/bookings', async (req, res) => {
-  try {
-    const bookings = await getBookings();
-    res.status(200).json(bookings);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Something went wrong while getting the list of bookings");
-  }
-})
+
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
