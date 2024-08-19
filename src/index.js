c8aad190cb6f7cc5579edcfc133e8631dce75ce0
Jeroen <147590275+roener88@users.noreply.github.com>
2024-08-19 11:27:29 +0200

    Added get bookings route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import getUsers from "./services/users/getUsers.js";
 
+// Booking
+import getBookings from "./services/bookings/getBookings.js";
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
+// User Routes
 app.get('/users', async (req, res) => {
   try {
     const users = await getUsers();
@@ -16,6 +20,18 @@ app.get('/users', async (req, res) => {
     res.status(500).send("Something went wrong while getting the list of users");
   }
 });
+
+
+// Booking Routes
+app.get('/bookings', async (req, res) => {
+  try {
+    const bookings = await getBookings();
+    res.status(200).json(bookings);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong while getting the list of bookings");
+  }
+})
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
